143f7547f3c8d5defeb064329d3bbff5e70a524c
Rayza-Souto <rayzasouto@outlook.com>
2022-11-01 22:02:51 -0300

    criando um método para smart tv

---
+++ src/SmartTv.java
@@ -0,0 +1,31 @@
+public class SmartTv {
+
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void ligar () {
+
+        ligada = true;
+
+    }
+
+    public void desligar () {
+
+        ligada = false;
+
+    }
+
+    public void aumentarVolume(){
+        //volume = volume +1
+        volume ++;
+
+    }
+
+    public void diminuirVolume(){
+        //volume = volume -1
+        volume --;
+
+    }
+
+}
